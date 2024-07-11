c5f7a2b0840add048ec713f05167f547bf596478
t7085 <t70858596@gmail.com>
2024-07-11 22:48:42 +0530

    finished

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+
+	public static void main String(args[])
+	{
+	 int n1 = 50;
+	 int n2 = 60;
+	 int sum = n1 + n2;
+	 System.out.println("Addition of two number is " + sum);
+	}
+}
