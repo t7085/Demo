e96c34063b0ca29fe0bf42614ea37113f1f97073
t7085 <t70858596@gmail.com>
2024-07-13 10:31:25 +0530

    finisher

--- addition.java
+++ addition.java
@@ -2,8 +2,11 @@ public class Addition {
 
 	public static void main String(args[])
 	{
-	 int n1 = 50;
-	 int n2 = 60;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entere first nunbers");
+	 int n1 = sc.nextInt();
+	 System.out.println("Entere second nunbers");
+	 int n2 = sc.nextInt();
 	 int sum = n1 + n2;
 	 System.out.println("Addition of two number is " + sum);
 	}
